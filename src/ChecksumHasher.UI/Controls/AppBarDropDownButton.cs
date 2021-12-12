34926ff8a40a226c187dc4bb9240bb5a2d3e9b6f
Kozmo Yi <kozmoyi@outlook.com>
2021-12-11 23:30:33 -0800

    ✨💄 Introduce AppBarDropDownButton for a better look and feel.

---
+++ src/ChecksumHasher.UI/Controls/AppBarDropDownButton.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace KozmoTech.ZenUtility.ChecksumHasher.UI.Controls;
+
+public sealed class AppBarDropDownButton : AppBarButton
+{
+    public AppBarDropDownButton()
+    {
+        DefaultStyleKey = typeof(AppBarDropDownButton);
+    }
+}
